47dbcbad78f35319a40cc8a093041536df1efc89
Devanshu Bhimwal <fullcools@gmail.com>
2026-06-12 16:53:44 +0200

    Add: maximum-nesting-depth-of-the-parentheses - submission-0

---
+++ Data Structures & Algorithms/maximum-nesting-depth-of-the-parentheses/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int currentDepth = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                currentDepth++;
+                maxDepth = Math.max(maxDepth, currentDepth);
+            }
+            if(s.charAt(i) == ')') {
+                currentDepth--;
+            }
+        }
+        return maxDepth;
+    }
+}
