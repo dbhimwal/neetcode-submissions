daa3f241bee5597c4481e5b8c8798656a8cac10b
Devanshu Bhimwal <fullcools@gmail.com>
2026-06-18 11:43:38 +0200

    Add: buildings-with-an-ocean-view - submission-3

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findBuildings(int[] heights) {
+        Stack<Integer> oceanViewBuilding = new Stack<>();
+
+        for(int i = 0; i < heights.length; i++) {
+            while(!oceanViewBuilding.isEmpty() && heights[oceanViewBuilding.peek()] <= heights[i]) {
+                oceanViewBuilding.pop();
+            }
+            oceanViewBuilding.push(i);
+        }
+        // System.out.println(oceanViewBuilding);
+        int[] result = new int[oceanViewBuilding.size()];
+        for(int i = oceanViewBuilding.size(); i > 0; i--) {
+            result[oceanViewBuilding.size() - 1] = oceanViewBuilding.pop();
+        }
+        return result;
+
+    }
+}
